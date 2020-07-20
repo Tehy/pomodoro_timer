ca957d83083542a6baca0765bbb58d7a67d36029
k1921 <k1921@student.jamk.fi>
2020-07-20 18:04:08 +0300

    Updates

---
+++ precache-manifest.d4dfd1c7d9638d9b6823cab92b4b7cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120b11bb4003b1210fb8b1969112f3ae",
+    "url": "/pomodoro_timer/index.html"
+  },
+  {
+    "revision": "a71efe076722b2c4f05f",
+    "url": "/pomodoro_timer/static/css/main.8321b25c.chunk.css"
+  },
+  {
+    "revision": "14fb19fec42b55b0d12e",
+    "url": "/pomodoro_timer/static/js/2.49c7d791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro_timer/static/js/2.49c7d791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a71efe076722b2c4f05f",
+    "url": "/pomodoro_timer/static/js/main.c36d3b6c.chunk.js"
+  },
+  {
+    "revision": "c9eaf55ed79156343d94",
+    "url": "/pomodoro_timer/static/js/runtime-main.cc49c19f.js"
+  }
+]);
